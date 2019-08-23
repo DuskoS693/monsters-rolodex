9f4460821195ca005b6d83cccc7a055ac96f9ee6
Dusko Stanisic <dusko474@gmail.com>
2019-08-23 18:07:29 +0200

    Updates

---
+++ precache-manifest.eb48745137b401b11d86e76f602a724b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17814a5f58f31f42b4a12dfb59e847ca",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "27ce5776d098b76ffb5b",
+    "url": "/github.io/monsters-rolodex/static/css/main.b8ddb92b.chunk.css"
+  },
+  {
+    "revision": "f8de0d181b1d54aa0809",
+    "url": "/github.io/monsters-rolodex/static/js/2.7d5a6575.chunk.js"
+  },
+  {
+    "revision": "27ce5776d098b76ffb5b",
+    "url": "/github.io/monsters-rolodex/static/js/main.760ad7c9.chunk.js"
+  },
+  {
+    "revision": "69320b197bead9dec0eb",
+    "url": "/github.io/monsters-rolodex/static/js/runtime~main.d002e000.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
